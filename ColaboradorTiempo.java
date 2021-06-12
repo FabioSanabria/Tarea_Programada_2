4264c1a4819511e9b142c5bb7089d909221e9fc9
Maria Paula Oviedo <mariapaula.oviedo@gmail.com>
2021-06-11 18:50:57 -0600

    modificacion de UsoRecurso y ColaboradorTiempo

--- ColaboradorTiempo.java
+++ ColaboradorTiempo.java
@@ -12,4 +12,35 @@ public class ColaboradorTiempo
     int idColaborador;
     // Tiempo en semanas que el colaborador dedicará a la tarea (se calcula con base el tiempo estimado de la tarea)
     double semanasColaborador;
+    
+    /**
+     * Constructor de la clase ColaboradorTiempo
+     * 
+     * La semanasColaborador se asigna inicialmente en 0
+     * 
+     * @param idColaborador El identificador del colaborador.
+     * 
+     */
+    public ColaboradorTiempo(int idColaborador){
+        this.idColaborador = idColaborador;
+        this.semanasColaborador = 0;
+    }
+    
+    // Métodos get y set
+    
+    public int getIdColaborador(){
+        return this.idColaborador;
+    }
+    
+    public double getSemanasColaborador(){
+        return this.semanasColaborador;
+    }
+    
+    public void setIdColaborador(int idColaborador){
+        this.idColaborador = idColaborador;
+    }
+    
+    public void setSemanasColaborador(int semanasColaborador){
+        this.semanasColaborador = semanasColaborador;
+    }
 }
--- UsoRecurso.java
+++ UsoRecurso.java
@@ -13,6 +13,54 @@ public class UsoRecurso
     double cantidadSolicitadaRecurso;
     // Cantidad asignada del recurso en una tarea
     double cantidadAsignadaRecurso;
-    // Porcentaje de utilización del recurso en la tarea
+    // Porcentaje de asignación del recurso en la tarea
     double porcentajeRecurso;
+    
+    /**
+     * Constructor de la clase UsoRecurso
+     * 
+     * La cantidadAsignadaRecurso y el porcentajeRecurso se inicializan en cero
+     * 
+     * @param idRecurso El identificador del recurso.
+     * @param cantidadSolicitadaRecurso La cantidad solicitada del recurso
+     * 
+     */
+    public void UsoRecurso(int idRecurso, double cantidadSolicitadaRecurso){
+        this.idRecurso = idRecurso;
+        this.cantidadSolicitadaRecurso = cantidadSolicitadaRecurso;
+        this.cantidadAsignadaRecurso = 0;
+        this.porcentajeRecurso = 0;
+    }
+    
+    // Métodos get y set
+    public int getIdRecurso(){
+        return this.idRecurso;
+    }
+    
+    public double getCantidadSolicitadaRecurso(){
+        return this.cantidadSolicitadaRecurso;
+    }
+    
+    public double getCantidadAsignadaRecurso(){
+        return this.cantidadAsignadaRecurso;
+    }
+    
+    public double getPorcentajeRecurso(){
+        return this.porcentajeRecurso;
+    }
+    
+    public void setIdRecurso(int idRecurso){
+        this.idRecurso = idRecurso;
+    }
+    
+    public void setCantidadSolicitadaRecurso(double cantidadSolicitadaRecurso){
+        this.cantidadSolicitadaRecurso = cantidadSolicitadaRecurso;
+        this.porcentajeRecurso = (this.cantidadAsignadaRecurso / this.cantidadSolicitadaRecurso) * 100;
+    }
+    
+    public void setCantidadAsignadaRecurso(double cantidadAsignadaRecurso){
+        this.cantidadAsignadaRecurso = cantidadAsignadaRecurso;
+        this.porcentajeRecurso = (this.cantidadAsignadaRecurso / this.cantidadSolicitadaRecurso) * 100;
+    }
+    
 }
